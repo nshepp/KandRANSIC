d2a7ba579aae0e77acf4898463433b8b6321fe67
nshepp <ngarwoodsheppard@gmail.com>
2014-06-04 14:03:59 -0400

    Float version with Header statement

--- Chapter1/FahrenheitCelsiusFloatingPoint.c
+++ Chapter1/FahrenheitCelsiusFloatingPoint.c
@@ -4,7 +4,9 @@
 
 int main()
 {
-	int fahr, celsius;
+    printf("Fahr  Cels\n");
+    
+    float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0;   	/* Lower limit of temperature table */
@@ -14,8 +16,8 @@ int main()
 	fahr = lower;
 	while (fahr <= upper)
 	{
-		celsius = 5 * (fahr-32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0/9.0) * (fahr-32);
+		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
