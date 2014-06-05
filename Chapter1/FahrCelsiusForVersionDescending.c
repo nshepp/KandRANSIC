b08651e16d5b2ecd45d7ce955d53474486597950
nshepp <ngarwoodsheppard@gmail.com>
2014-06-05 18:30:16 -0400

    Descending order of For fahr/cels
    finished exercise 1-5

---
+++ Chapter1/FahrCelsiusForVersionDescending.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table  */
+
+int main()
+{
+    int fahr;
+
+	for (fahr = 300; fahr >= 0; ) 
+    {
+		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr-32));
+        fahr = fahr - 20;
+	}
+}
