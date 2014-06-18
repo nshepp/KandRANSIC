8804c015f46ad0c3a1e98626fec7b474566f3606
Andrew Sheppard <ajtsheppard@gmail.com>
2014-06-18 14:10:34 -0400

    Prints lines of text above 80 chars
    Functions

---
+++ Chapter1/Ex1.17.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define MAXLINE 1000     /* Maximum length of an input string */
+
+int mygetline(char line[], int maxline);
+
+/* print all lines longer than MAXLINE */
+
+main()
+{
+  int len;                       /* current line length */
+  char line[MAXLINE+1];          /* current input line plus '\0' */
+  
+  while ((len = mygetline(line, MAXLINE)) != EOF)
+    if (len > 80)
+      printf("%s\n", line);
+  return 0;
+}
+
+/* getline: read a line into s, return length */
+int mygetline(char s[], int lim)
+{
+  int c, i;
+  
+  for (i = 0; (c = getchar()) !=EOF && c!='\n'; ++i)
+    if (i < lim)
+      s[i] = c;
+  if (i<lim)
+    lim = i;
+  s[lim] = '\0';
+  if (c == EOF)
+    return EOF;
+  else
+    return i;
+}
