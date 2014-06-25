e33447130eabe8142eafb5c54372dc74b24ce864
Andrew Sheppard <ajtsheppard@gmail.com>
2014-06-25 18:54:01 -0400

    Ostensibly finished ex1.21.c, needs further testing
    bloater10.txt as test case

--- Chapter1/Ex1.21.c
+++ Chapter1/Ex1.21.c
@@ -3,9 +3,9 @@
 #define TAB_WIDTH 4
 
 int mygetline(char line[], int maxline);
-void detab(char s[], int tab_width);
+void entab(char s[], int tab_width);
 
-/* detabs a string */
+/* entabs a string */
 
 main()
 {
@@ -15,7 +15,7 @@ main()
   while ((len = mygetline(line, MAXLINE)) != EOF)
     if (len>0)
     {
-       detab(line, TAB_WIDTH);
+       entab(line, TAB_WIDTH);
        printf("%s\n", line);
     }
   return 0;
@@ -38,43 +38,68 @@ int mygetline(char s[], int lim)
     return i;
 }
 
-void detab(char s[], int tab_width)
+void entab(char s[], int tab_width)
 {
-  int i, j, k, len, spaces;
+  int i, j, idx, len, tabs, spaces, whitespaces;
   char c;
   
   /* Finds string length 'len' */
   len = 0;
   while (s[len]!='\0') len++;
   
-  /* detabs */
-  i = 0;
-  while (i < len)
+  /* entabs */
+  tabs = spaces = whitespaces = 0;
+  i = len-1;
+  while (i >= 0)
   {
     c = s[i];
     
-    if(c == '\t') 
+    if(c == ' ')
     {
-      spaces = (i/tab_width + 1) * (tab_width) - i - 1;
+      /* is previous character on a tab stop */
+      if ((i+1)%tab_width == 0)
+        tabs++;
+      else if (tabs == 0) 
+        spaces++;
       
-      /* shift remainder of string to the right */
-      j = len;
-      while(j >= i + 1)
-      {
-        s[j+spaces] = s[j];
-        
-        j--;
-      }
-      
-      /* backfill with blanks */
-      for(k = 0; k <= spaces; k++) s[i+k] = ' ';
-      
-      i += spaces + 1;
-      
-      len += spaces;
+      whitespaces++;
     }
-    else
-      i++;
+    else if (tabs!=0 || spaces!=0) /* new word found */
+    {
+      /* writes tabs over spaces */
+      j = 0;
+      while (j++ < tabs) s[i+j+1] = '\t';
+      
+      /* index of start character for previous word */
+      idx = i + whitespaces + 1;
+      
+      /* compress white space into tabs and spaces */
+      j = idx;
+      do
+      {
+        c = s[j];
+        
+        s[j-(whitespaces-tabs-spaces)] = c;
+        
+        j++;
+      }
+      while (c != '\0');
+      
+      tabs = spaces = whitespaces = 0;  
+    }
+    
+    i--;
   }
-  
-}
+}    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
