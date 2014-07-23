2468802bbf184db3676b2d31f2b2b4710cc256af
Andrew Sheppard <ajtsheppard@gmail.com>
2014-07-23 13:32:02 -0400

    Onto Ch2, finished Ex2.1

---
+++ Chapter2/Ex2.1/Ex2.1/main.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    printf("INT_MAX %d\n", INT_MAX);
+    printf("INT_MIN %d\n", INT_MIN);
+    printf("LONG_MAX %ld\n", LONG_MAX);
+    printf("LONG_MIN %ld\n", LONG_MIN);
+    printf("SHRT_MAX %d\n", SHRT_MAX);
+    printf("SHRT_MIN %d\n", SHRT_MIN);
+    
+    printf("CHAR_BIT %d\n", CHAR_BIT);
+    printf("CHAR_MAX %d\n", CHAR_MAX);
+    printf("CHAR_MIN %d\n", CHAR_MIN);
+    printf("SCHAR_MAX %d\n", SCHAR_MAX);
+    printf("SCHAR_MIN %d\n", SCHAR_MIN);
+    printf("UCHAR_MAX %d\n", UCHAR_MAX);
+    printf("UINT_MAX %d\n", UINT_MAX);
+    printf("ULONG_MAX %lu\n", ULONG_MAX);
+    printf("USHRT_MAX %d\n", USHRT_MAX);
+
+    
+    printf("FLT_RADIX %d\n", FLT_RADIX);
+    printf("FLT_ROUNDS %d\n", FLT_ROUNDS);
+    printf("FLT_DIG %d\n", FLT_DIG);
+    printf("FLT_EPSILON %f\n", FLT_EPSILON);
+    printf("FLT_MANT_DIG %d\n", FLT_MANT_DIG);
+    printf("FLT_MAX %f\n", FLT_MAX);
+    printf("FLT_MAX_EXP %d\n", FLT_MAX_EXP);
+    printf("FLT_MIN %f\n", FLT_MIN);
+    printf("FLT_MIN_EXP %d\n", FLT_MIN_EXP);
+    printf("DBL_DIG %d\n", DBL_DIG);
+    printf("DBL_EPSILON %f\n", DBL_EPSILON);
+    printf("DBL_DIG %d\n", DBL_MANT_DIG);
+    printf("DBL_MAX %f\n", DBL_MAX);
+    printf("DBL_MAX_EXP %d\n", DBL_MAX_EXP);
+    printf("DBL_MIN %f\n", DBL_MIN);
+    printf("DBL_MIN_EXP %d\n", DBL_MIN_EXP);
+
+    
+    return 0;
+}
+
