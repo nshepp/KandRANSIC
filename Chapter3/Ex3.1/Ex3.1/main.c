d8773b8a9ac89436fbbc88a900e70cf23265d4dd
Andrew Sheppard <ajtsheppard@gmail.com>
2014-08-12 13:07:34 -0400

    Ex3.1 working with answer text and tests
    requires further testing

--- Chapter3/Ex3.1/Ex3.1/main.c
+++ Chapter3/Ex3.1/Ex3.1/main.c
@@ -5,6 +5,7 @@
 //  Created by Andrew Sheppard on 8/7/14.
 //  Copyright (c) 2014 Andrew Sheppard. All rights reserved.
 //
+#include <assert.h>
 
 int binsearch(int x, int v[], int n);
 int binsearch1(int x, int v[], int n);
@@ -13,15 +14,43 @@ int binsearch1(int x, int v[], int n);
 
 int main(int argc, const char * argv[])
 {
-    int i, n = 5, x = 5, result;
-    int v[] = {1, 3, 5, 7, 9};
+    int i, n = 5, result;
+    int v[] = {2, 3, 5, 7, 9};
     
     for (i=0; i<argc; i++)
         printf("argv[%d] = %s\n", i, argv[i]);
 
-    result = binsearch(x, v, n);
-    
+    // Test < lower bound.
+    result = binsearch1(1, v, n);
+    assert(result==-1);
     printf("Valued returned: %d\n", result);
+    
+    // Test > high bound.
+    result = binsearch1(10, v, n);
+    assert(result==-1);
+    printf("Valued returned: %d\n", result);
+    
+    // Test equal v[0] bound.
+    result = binsearch1(2, v, n);
+    assert(result==0);
+    printf("Valued returned: %d\n", result);
+
+    // Test equal v[hi] bound.
+    result = binsearch1(9, v, n);
+    assert(result==4);
+    printf("Valued returned: %d\n", result);
+
+    // Test != any v[].
+    result = binsearch1(4, v, n);
+    assert(result==-1);
+    printf("Valued returned: %d\n", result);
+
+    // Test == any of the middle v[] values.
+    result = binsearch1(5, v, n);
+    assert(result==2);
+    printf("Valued returned: %d\n", result);
+
+    return 0;
 }
 
 int binsearch(int x, int v[], int n)
@@ -43,21 +72,19 @@ int binsearch(int x, int v[], int n)
     return -1; /* no match */
 }
 
-
 int binsearch1(int x, int v[], int n)
 {
     int low, high, mid;
 
     low = 0;
     high = n - 1;
-    while (low <= high)
+    while (low<=high && v[(mid = low + (high-low) / 2)] != x)
     {
-        mid = (low + high) / 2;
-        if (x < v[mid])
+        if (v[mid] > x)
             high = mid - 1;
-        else if (x == v[mid]) /* found match */
-            return mid;
+        else
+            low = mid + 1;
     }
     
-    return -1; /* no match */
+    return  (v[mid] == x) ? mid : -1;
 }
